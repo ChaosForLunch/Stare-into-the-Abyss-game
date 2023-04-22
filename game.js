8bedb97bb7de9db3a45d730341c998ad335ae956
Chaos For Lunch <131422640+ChaosForLunch@users.noreply.github.com>
2023-04-21 18:48:40 -0600

    2023-04-21 ... Updated game.js

--- game.js
+++ game.js
@@ -1,30 +1,88 @@
+window.onload = function() {
+  const container = document.querySelector('.container');
+  const player = document.querySelector('.player');
+
+document.addEventListener('keydown', (e) => {
+  const arrowKeys = ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"];
+  if (arrowKeys.includes(e.key)) {
+    e.preventDefault();
+    const containerWidth = container.offsetWidth;
+    const xPos = (player.offsetLeft - containerWidth / 2) / 10;
+    const yPos = (window.innerHeight / 2 - player.offsetTop) / 10;
+    let xMovement = 0;
+    let yMovement = 0;
+    if (e.key === "ArrowLeft") {
+      xMovement = -10;
+    } else if (e.key === "ArrowRight") {
+      xMovement = 10;
+    } else if (e.key === "ArrowUp") {
+      yMovement = -10;
+    } else if (e.key === "ArrowDown") {
+      yMovement = 10;
+    }
+    player.style.left = `${player.offsetLeft + xMovement}px`;
+    player.style.top = `${player.offsetTop + yMovement}px`;
+    container.style.backgroundPosition = `${xPos + xMovement}px ${yPos + yMovement}px`;
+  }
+});
+}  
+
 // Define the canvas and get the context
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-// Set the canvas dimensions
-canvas.width = 800;
+// Set the Canvas dimensions
+canvas.width = 900;
 canvas.height = 600;
+
+// Load the Background image
+const backgroundImage = new Image();
+backgroundImage.src = "images/background.png";
 
 // Player object
 const player = {
   x: 50,
   y: 50,
-  width: 50,
-  height: 50,
-  speed: 5,
+  width: 150,
+  height: 150,
+  speed: 2,
   jumping: false,
   rightPressed: false,
   leftPressed: false,
 };
 
+// Coin object
+const coin = {
+  x: 300,
+  y: 300,
+  width: 50,
+  height: 50,
+  image: new Image(),
+}
+
 // Platform object
 const platform = {
-  x: 200,
-  y: 400,
+  x: 220,
+  y: 405,
   width: 200,
-  height: 20,
+  height: 30,
+  image: new Image(),
 };
+
+// Load the Player image
+const playerImg = new Image();
+playerImg.src = "images/player.png";
+
+// Load the Coin image
+const coinImg = new Image();
+coinImg.src = "images/coin.png";
+
+// Load the Platform image
+const platformImg = new Image();
+platformImg.src = "images/platform.png";
+
+// Score variable
+let score = 0;
 
 // Keyboard input event listeners
 document.addEventListener("keydown", keyDownHandler);
@@ -48,14 +106,56 @@ function keyUpHandler(event) {
   }
 }
 
-// Game loop
-function gameLoop() {
-  // Clear the canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+let playerX = 400; // starting x position of the player
 
-  // Draw the player
-  ctx.fillStyle = "#FFA500";
-  ctx.fillRect(player.x, player.y, player.width, player.height);
+ // handle player movement with keyboard input
+ document.addEventListener('keydown', (e) => {
+  if (e.code === 'ArrowLeft') {
+    playerX -= 10; // move player left
+  } else if (e.code === 'ArrowRight') {
+    playerX += 10; // move player right
+  }
+});
+
+// Function to draw the Coin
+function drawCoin() {
+  ctx.drawImage(coinImg, coin.x, coin.y, coin.width, coin.height);
+}
+
+// Function to draw the Platform
+function drawPlatform() {
+  ctx.drawImage(platformImg, platform.x, platform.y, platform.width, platform.height);
+}
+
+// Check for collision Player and Platform
+function checkCollisionPlatform(player, platform) {
+  if (player.x < platform.x + platform.width &&
+      player.x + player.width > platform.x &&
+      player.y + player.height > platform.y &&
+      player.y + player.height < platform.y + platform.height) {
+    return true;
+  }
+  return false;
+}
+
+// Check for collision Player and Coin
+function checkCollisionCoin(player, coin) {
+  if (player.x < coin.x + coin.width &&
+      player.x + player.width > coin.x &&
+      player.y + player.height > coin.y &&
+      player.y + player.height < coin.y + coin.height) {
+    return true;
+  }
+  return false;
+}
+
+// Game Loop
+function gameLoop() {
+  // Draw the background image
+  ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+
+  // Draw the Player image
+  ctx.drawImage(playerImg, player.x, player.y, player.width, player.height);
 
   // Move the player
   if (player.rightPressed) {
@@ -74,22 +174,49 @@ function gameLoop() {
     player.y += 5;
   }
 
-  // Draw the platform
-  ctx.fillStyle = "#9932CC";
-  ctx.fillRect(platform.x, platform.y, platform.width, platform.height);
+  // Load the Coin image
+  coin.image.src = "images/coin.png";
+
+  // Load the Platform image
+  platform.image.src = "images/platform.png";
+
+  // Increment Score for each collision with the coin
+  if (player.x < coin.x + coin.width &&
+  player.x + player.width > coin.x &&
+  player.y + player.height > coin.y &&
+  player.y + player.height < coin.y + coin.height) {
+    player.jumping = false;
+    player.y = coin.y - player.height;
+    }
+
+  // In the gameLoop() function, replace the line that draws the Platform rectangle with this:
+  drawPlatform();
 
   // Check for collisions between player and platform
   if (player.x < platform.x + platform.width &&
-      player.x + player.width > platform.x &&
-      player.y + player.height > platform.y &&
-      player.y + player.height < platform.y + platform.height) {
+    player.x + player.width > platform.x &&
+    player.y + player.height > platform.y &&
+    player.y + player.height < platform.y + platform.height) {
     player.jumping = false;
     player.y = platform.y - player.height;
   }
 
-  // Request the next animation frame
-  window.requestAnimationFrame(gameLoop);
-}
+  // Score variable
+  let score = 0;
 
-// Start the game loop
-window.requestAnimationFrame(gameLoop);
+  // Function to draw the score on the canvas
+  function drawScore() {
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText("Score: " + score, 15, 35);
+  }
+
+  // Draw the score on the canvas
+  drawScore();
+
+    // Request the next animation frame
+    window.requestAnimationFrame(gameLoop);
+  }
+
+  // Start the game loop
+  window.requestAnimationFrame(gameLoop);
